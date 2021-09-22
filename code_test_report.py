d686317e2029105d3474fa5f7bdb1edaf59a95d3
Adam La Morre <adam.lamorre@snyk.io>
2021-09-22 09:49:11 -0700

    first commit

---
+++ code_test_report.py
@@ -0,0 +1,24 @@
+import json
+import pprint
+
+with open('code_tests.json') as f:
+    code_json = json.load(f)
+
+results = code_json['runs'][0]['results']
+
+simple_results = []
+for result in results:
+    simple_result = {}
+    simple_result['type'] = result['ruleId']
+    simple_result['level'] = result['level']
+    simple_result['message'] = result['message']['text']
+    simple_result['file'] = result['locations'][0]['physicalLocation']['artifactLocation']['uri']
+    simple_result['lines'] = '{} - {}'.format(
+        result['locations'][0]['physicalLocation']['region']['startLine'],
+        result['locations'][0]['physicalLocation']['region']['endLine']
+    )
+    simple_results.append(simple_result)
+
+
+pprint.pprint(simple_results)
+print(len(simple_results))
